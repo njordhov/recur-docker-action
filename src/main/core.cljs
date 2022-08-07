347eb5b2097795fbcf49c12cda8d354ac8f6a8ec
Terje Norderhaug <terje@in-progress.com>
2022-08-06 17:15:03 -0700

    Update core.cljs

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,13 +1,1 @@
-(ns action.core
-  (:require 
-   [clojure.tools.cli 
-    :refer [parse-opts]]
-   [clojure.string :as string]
-   [shadow.resource :as rc]))
 
-(defn exit [status msg]
-  (println msg)
-  (.exit js/process status))
-
-(defn main [& args]
-  (println "Hello"))
